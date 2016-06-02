f2452568b3f9343f30b848f6cf5eeca489b462ad
Michael Comella <michael.l.comella@gmail.com>
2016-06-02 10:44:12 -0700

    replace w/ MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject reader-links "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51" :scope "provided"]
