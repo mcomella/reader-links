0e2242592b54f20337c5ec814f2ba1b62f5f5e72
Michael Comella <michael.l.comella@gmail.com>
2016-06-03 20:43:12 -0700

    Create combined app component.

--- src/cljs/reader_links/core.cljs
+++ src/cljs/reader_links/core.cljs
@@ -3,23 +3,30 @@
 
 (enable-console-print!)
 
-(defonce url (r/atom ""))
-
 (defn on-input-submit [e & _]
   (.preventDefault e)
   (prn _))
 
-(defn url-input []
+(defn url-input [value]
   [:div
    [:form {:className "url-input"
            :onSubmit on-input-submit}
     [:input {:type "text"
-             :value @url
+             :value @value
              :placeholder "URL to readerify"
-             :on-change #(reset! url (-> % .-target .-value))
-             :size 80}]
+             :on-change #(reset! value (-> % .-target .-value))
+             :size 80}] ; TODO: in css
     [:input {:type "submit"
              :value "Load"}]]])
 
-(r/render [url-input]
+(defn reader [title]
+  [:div "testing"])
+
+(defn app []
+  (let [url (r/atom "")]
+    [:div
+     [url-input url]
+     [reader]]))
+
+(r/render [app]
           (.getElementById js/document "xyz-mcomella-app"))
