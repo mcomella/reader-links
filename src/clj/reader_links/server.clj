6e1ba1d247e0b32e7d54842d19d2d241668bb4a3
Michael Comella <michael.l.comella@gmail.com>
2016-06-03 21:12:20 -0700

    Slurp a specific path on the server.

--- src/clj/reader_links/server.clj
+++ src/clj/reader_links/server.clj
@@ -9,17 +9,15 @@
             [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
-(def path "https://en.wikipedia.org/wiki/%EC%95%84%EC%9D%B4%EC%9C%A0")
-
 (defroutes routes
   (GET "/" _
     {:status 200
      :headers {"Content-Type" "text/html; charset=utf-8"}
      :body (io/input-stream (io/resource "public/index.html"))})
-  (GET "/slurp" []
+  (GET "/slurp" [url]
     {:status 200
      :headers {"Content-Type" "text/xml; charset=utf-8"}
-     :body (slurp path)})
+     :body (slurp url)})
   (resources "/"))
 
 (def http-handler
