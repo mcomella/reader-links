a59e9669500530674db24a170f6799f017c24343
Michael Comella <michael.l.comella@gmail.com>
2016-06-10 13:36:13 -0700

    it kind of works

--- src/cljs/reader_links/core.cljs
+++ src/cljs/reader_links/core.cljs
@@ -1,6 +1,7 @@
 (ns reader-links.core
   (:require [reader-links.reader :refer [readerify]]
             [reader-links.dom :as dom]
+            [clojure.string :as str]
             [reagent.core :as r]
             [ajax.core :refer [GET]]))
 
@@ -34,17 +35,50 @@
      [:div {:dangerouslySetInnerHTML
             #js {:__html content}}]])) ; TODO: make safe.
 
-(defn link-node-to-markup [node]
+(defn link-node-to-markup [node ctxt]
   (let [title (.-innerText node)
+        [before after] (str/split ctxt title)
         href (.-href node)]
-    [:div
-     [:a {:href href} title]]))
+    [:li
+     [:em before]
+      [:a {:href href} title]
+      [:em after]]))
+
+(defonce ldev (atom nil))
+
+; TODO: make not awful
+; TODO: can re-use?
+(defn get-start-of-sentence-ind [s]
+  (let [[before after] (str/split (str/reverse s)
+                                  #"\s+[!.,?]\S"
+                                  2)]
+    (if (empty? after) ; TODO: off-by one
+      0
+      (+ (count after)
+        2)))) ; TODO: explain
+
+; TODO: re-use of regex
+(defn get-end-of-sentence-ind [s]
+  (let [[before-end-of-sentence _] (str/split s #"\S[!.,?]\s+" 2)]
+    (+ (count before-end-of-sentence)
+       2))) ; for characters in split. TODO: 2 or 3?
+
+(defn get-containing-sentence-from-link-node [link-node]
+  (let [ctxt (-> link-node .-parentNode .-innerText)
+        title (.-innerText link-node)
+        [before after] (str/split ctxt title)
+        working-after (str title after)
+        start-of-sent-ind (get-start-of-sentence-ind before)
+        end-of-sent-ind (get-end-of-sentence-ind working-after)] ; TODO: may end w/ sentence
+    (subs ctxt start-of-sent-ind (+ (count before) ; TODO: expl offset
+                                    end-of-sent-ind))))
 
 (defn links []
   (let [{:keys [content]} @article
         links (dom/get-all-links content)
-        links-markup-seq (map link-node-to-markup links)
-        links-markup (into [:div] links-markup-seq)]
+        link-ctxt (map get-containing-sentence-from-link-node links)
+        links-markup-seq (map link-node-to-markup links link-ctxt)
+        links-markup (into [:ul] links-markup-seq)]
     [:div
      [:hr]
      [:h2 "Some links you may have missed..."]
