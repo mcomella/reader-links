6373f33be05a42b8a825db20f5f0e028d5415d15
Michael Comella <michael.l.comella@gmail.com>
2016-06-30 08:53:11 -0700

    add onReaderContentReady w/ hack.
    I tried to use the AboutReaderContentReady event but I never received
    the event.

--- src/reader.js
+++ src/reader.js
@@ -2,9 +2,15 @@ const ArticleLinks = require('./article_links.js').ArticleLinks;
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const container = document.createElement('div');
-document.body.appendChild(container);
-ReactDOM.render(
-  <ArticleLinks/>,
-  container
-);
+function onReaderContentReady() {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  ReactDOM.render(
+    <ArticleLinks/>,
+    container
+  );
+}
+
+// TODO: Use event like AboutReaderContentReady, or
+// at least wait for reader dom structure to appear.
+window.setTimeout(onReaderContentReady, 1000);
