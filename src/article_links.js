74b538f348d2b9c1efe4d82c25ac29eed71209ab
Michael Comella <michael.l.comella@gmail.com>
2016-06-30 07:40:30 -0700

    add stencil article-link container.

---
+++ src/article_links.js
@@ -0,0 +1,43 @@
+const React = require('react');
+
+// TODO: names.
+const Link = React.createClass({
+  render: function () {
+    return (
+      <div className="mcomella-xyz-link">
+        Here we put an article.
+      </div>
+    );
+  }
+});
+
+const ArticleList = React.createClass({
+  render: function () {
+    const articleNodes = this.props.articles.map((article) => {
+      return (
+        <Link article={article}/> // TODO: pass less than article?
+      );
+    });
+
+    return (
+      <div className="mcomella-xyz-article-list">
+        {articleNodes}
+      </div>
+    );
+  },
+});
+
+const ArticleLinks = React.createClass({
+  getInitialState: function () {
+    return { articles: [] };
+  },
+  render: function () {
+    return (
+      <div className="mcomella-xyz-article-links">
+        <h1>Some links you may have missed...</h1>
+        <ArticleList articles={this.state.articles}/>
+      </div>
+    );
+  },
+});
+exports.ArticleLinks = ArticleLinks;
--- src/reader.js
+++ src/reader.js
@@ -1,9 +1,10 @@
+const ArticleLinks = require('./article_links.js').ArticleLinks;
 const React = require('react');
 const ReactDOM = require('react-dom');
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <ArticleLinks/>,
   container
 );
